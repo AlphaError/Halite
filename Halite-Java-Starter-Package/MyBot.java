a2915e5ffd339399cb73faafb69c1cdefde61b6b
Stewart Hughes <stewvhughes@gmail.com>
2016-11-22 13:09:28 -0500

    Redo of Halite Code
    --Fixed issue where cells surrounded by friendly cells would stay still
    indefinitely

--- Halite-Java-Starter-Package/MyBot.java
+++ Halite-Java-Starter-Package/MyBot.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MyBot {
     public static void main(String[] args) throws java.io.IOException {
@@ -6,7 +7,9 @@ public class MyBot {
         int myID = iPackage.myID;
         GameMap gameMap = iPackage.map;
 
-        Networking.sendInit("MyJavaBot");
+        Networking.sendInit("Error Bot");
+
+        Random rand = new Random();
 
         while(true) {
             ArrayList<Move> moves = new ArrayList<Move>();
@@ -17,8 +20,57 @@ public class MyBot {
                 for(int x = 0; x < gameMap.width; x++) {
                     Site site = gameMap.getSite(new Location(x, y));
                     if(site.owner == myID) {
-                        Direction dir = Direction.randomDirection();
-                        moves.add(new Move(new Location(x, y), dir));
+
+                        boolean movedPiece = false;
+                        int hasFriend = 0;
+                        int hasEnemy = 0;
+                        for(Direction d : Direction.CARDINALS) {
+                            if(gameMap.getSite(new Location(x, y), d).owner != myID &&
+                            gameMap.getSite(new Location(x, y), d).strength < gameMap.getSite(new Location(x, y)).strength) {
+                                moves.add(new Move(new Location(x, y), d));
+                                movedPiece = true;
+                                break;
+                            }
+                            if(gameMap.getSite(new Location(x, y), d).owner == myID) { //adding counters to calculate the amount of friendly and enemy squares based on the current square
+                                hasFriend += 1;
+                            }
+                            if(gameMap.getSite(new Location(x, y), d).owner != myID) {
+                                hasEnemy += 1;
+                            }
+                        }
+                        
+                        Direction dNew = null;
+                        Direction dOld = null;
+                        for(Direction e : Direction.CARDINALS) {
+                            if(gameMap.getSite(new Location(x, y), e).owner != myID &&
+                            gameMap.getSite(new Location(x, y), e).strength >= gameMap.getSite(new Location(x, y)).strength) {
+                                e = dNew;
+                                if(gameMap.getSite(new Location(x, y), dNew).strength < gameMap.getSite(new Location(x, y), dOld).strength) {
+                                    dOld = dNew;
+                                } //finding the lowest enemy square in the vacinity
+                            }
+                        }
+                        if(!movedPiece && gameMap.getSite(new Location(x, y), dOld).strength > gameMap.getSite(new Location(x, y)).strength) {
+                            //if the weakest enemy square is still larger than us we stay still
+                            moves.add(new Move(new Location(x, y), Direction.STILL));
+                            movedPiece = true;
+                        }
+                        
+                        if(hasFriend >= 4) {
+                           if(!movedPiece && gameMap.getSite(new Location(x, y)).strength < gameMap.getSite(new Location(x, y)).production * 5) {
+                               moves.add(new Move(new Location(x, y), Direction.STILL));
+                               movedPiece = true;
+                           }
+
+                           if(!movedPiece) {
+                               moves.add(new Move(new Location(x, y), rand.nextBoolean() ? Direction.SOUTH : Direction.WEST));
+                               movedPiece = true;
+                           }
+                        }
+                        
+                        if(!movedPiece) { // fall back plan
+                            moves.add(new Move(new Location(x, y), Direction.STILL));
+                        }
                     }
                 }
             }
