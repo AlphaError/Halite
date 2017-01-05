2ecfeb63b9cefa670fc4d922d513bc3454dcdb95
Stewart Hughes <stewvhughes@gmail.com>
2017-01-05 10:46:00 -0500

    A Lot of Changes
    ~Currently an unfinished version of code --> using an array/array list
    to track multiple moves ahead in 1 direction, particually to take the
    best/shortest path to the edge of your pieces.

--- Halite-Java-Starter-Package/MyBot.java
+++ Halite-Java-Starter-Package/MyBot.java
@@ -1,3 +1,4 @@
+//C:\Users\stewv\Documents\GitHub\Halite\Halite-Java-Starter-Package>runGame.bat
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -16,10 +17,27 @@ public class MyBot {
 
             gameMap = Networking.getFrame();
 
+            int frameCounter = 0;
+            int productionCount = 5;
+            Direction gameDirection1 = null;
+            Direction gameDirection2 = null;
             for(int y = 0; y < gameMap.height; y++) {
                 for(int x = 0; x < gameMap.width; x++) {
                     Site site = gameMap.getSite(new Location(x, y));
                     if(site.owner == myID) {
+                        if(frameCounter <= 100) {
+                            productionCount = 6;
+                            gameDirection1 = Direction.NORTH;
+                            gameDirection2 = Direction.EAST;
+                        }
+                        if(frameCounter > 100 && frameCounter <= 250) {
+                            productionCount = 7;
+                            gameDirection1 = Direction.SOUTH;
+                            gameDirection2 = Direction.WEST;
+                        }
+                        if(frameCounter > 250) {
+                            productionCount = 5;
+                        }
                         
                         boolean movedPiece = false;
                         int hasFriend = 0;
@@ -58,13 +76,43 @@ public class MyBot {
                             movedPiece = true;
                         }
                         
+                        
+                        //Direction fNew = null;
+                        int numToEnemy = 0;
+                        int lastNumEnemy = 0;
+                        int numEnemyCount = 0;
+                        Direction bestF = null;
                         if(hasFriend >= 4) { //friendly code
-                            if(!movedPiece && gameMap.getSite(new Location(x, y)).strength < gameMap.getSite(new Location(x, y)).production * 6) {
+                            for (Direction f : Direction.CARDINALS) {
+                                ArrayList<Direction> dirs = new ArrayList<Direction>();
+                                //Direction[] dirs = {};
+                                dirs.add(f);
+                                int arrayCount = 1;
+                                numEnemyCount += 1;
+                                Location newLoc = new Location(x, y);
+                                for (int a = 0; arrayCount >= a; a++) {
+                                    newLoc = gameMap.getSite(new Location(x, y), dirs[a]);
+                                }
+                                    while(gameMap.getSite(new Location(x, y), newLoc).owner == myID) {
+                                       dirs.add(f); //adding 1 more move in that direction...?
+                                       arrayCount += 1;
+                                       numToEnemy += 1;
+                                    }
+                                if (numEnemyCount < 1) {
+                                    lastNumEnemy = numToEnemy; //factoring in for the very first instance of the fNew
+                                }
+                                if (numEnemyCount >= 1 && numToEnemy < lastNumEnemy) {
+                                    lastNumEnemy = numToEnemy; //making sure the variable will be at it's lowest for the next loop
+                                    bestF = f; //setting the direction
+                                }
+                            }
+                            if(!movedPiece && gameMap.getSite(new Location(x, y)).strength < gameMap.getSite(new Location(x, y)).production * productionCount) {
                                moves.add(new Move(new Location(x, y), Direction.STILL));
                                movedPiece = true;
                             }
                             if(!movedPiece) {
-                               moves.add(new Move(new Location(x, y), rand.nextBoolean() ? Direction.SOUTH : Direction.WEST));
+                               //moves.add(new Move(new Location(x, y), rand.nextBoolean() ? gameDirection1 : gameDirection2));
+                               moves.add(new Move(new Location(x, y), bestF));
                                movedPiece = true;
                             }
                         }
@@ -78,6 +126,10 @@ public class MyBot {
                         //if(movedPiece && gameMap.getSite(new Location(x, y)).strength >= 255) { //fallback #2 for it
                           //  moves.add(new Move(new Location(x, y), Direction.randomDirection()));
                         //}
+                        
+                        if (movedPiece) {
+                            frameCounter += 1;
+                        }
                     }
                 }
             }
