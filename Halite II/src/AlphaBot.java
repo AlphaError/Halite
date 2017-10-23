76ba4c753c983f6884397d9d19ba11e747009795
Stewart Hughes <stewvhughes@gmail.com>
2017-10-23 15:38:35 -0400

    Eclipse Workspace Reorganization

---
+++ Halite II/src/AlphaBot.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Alpha Error - Stewart H.
+ * -For 2 Sigma's Halite II competition
+ */
+public class AlphaBot {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
